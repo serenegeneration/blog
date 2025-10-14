af5dab1b02c3fcfafcce2b80ca0d71acc467a432
serenegeneration <serenegeneration@gmail.com>
2025-10-14 14:46:40 +0100

    Update login.js

--- login.js
+++ login.js
@@ -42,7 +42,7 @@ function validateForm(event) {
         
         setTimeout(function () {
             loader.style.display = 'none';
-            window.location.href = 'blog/Blog.html';
+            window.location.href = 'https://serenegeneration.github.io/blog/Blog.html';
         }, 2000);
     }
 }
@@ -59,3 +59,4 @@ function makeValid(element) {
     element.style.border = "2px solid green";
 
 }
+
